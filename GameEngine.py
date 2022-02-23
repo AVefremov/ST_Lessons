4d4069a49e568d907644433ef02dca3544bc02aa
Ефремов Андрей <a.v.efremov@systtech.ru>
2022-02-23 18:09:51 +0200

    Lesson 2, add two classes

---
+++ GameEngine.py
@@ -0,0 +1,83 @@
+from random import randint
+
+
+class BaseGame:
+    def __init__(self):
+        self.hidden_number = randint(1, 10)
+        self.incorrect_lower_answer = 'Ответ, если число меньше'
+        self.incorrect_higher_answer = 'Ответ, если число больше'
+        self.start_game_message = 'Сообщение в начале игры'
+
+    def get_answer(self):
+        return 'Win'
+
+    def start_game(self):
+        print(self.start_game_message)
+        while True:
+            result = self.get_answer()
+            match result:
+                case 'Less':
+                    print(self.incorrect_lower_answer)
+                case 'More':
+                    print(self.incorrect_higher_answer)
+                case 'Win':
+                    break
+        print(f'Число угадано, ответ: {self.hidden_number}. Конец игры')
+
+
+class PlayerVsGame(BaseGame):
+    def __init__(self):
+        super().__init__()
+        self.incorrect_lower_answer = 'Введенное число меньше загаданного. попробуйте другой вариант.'
+        self.incorrect_higher_answer = 'Введенное число больше загаданного. попробуйте другой вариант.'
+        self.start_game_message = 'Я загадал целое число от 1 до 10. Попробуйте его угадать.'
+
+    def get_answer(self):
+        while True:
+            answer = input(r"Введите целое число от 1 до 10: ")
+            if not answer.isnumeric():
+                continue
+            answer = int(answer)
+            if answer > 10 or answer < 1:
+                continue
+            if answer < self.hidden_number:
+                result = 'Less'
+                break
+            elif answer > self.hidden_number:
+                result = 'More'
+                break
+            else:   # answer == self.hidden_number
+                result = 'Win'
+                break
+        return result
+
+
+class GameVsPlayer(BaseGame):
+    def __init__(self):
+        super().__init__()
+        self.range_start = 1
+        self.range_end = 10
+        self.incorrect_lower_answer = 'Тогда я загадаю число побольше'
+        self.incorrect_higher_answer = 'Тогда я загадаю число поменьше'
+        self.start_game_message = 'Загадайте целое число от 1 до 10. Я попробую его угадать.'
+
+    def get_answer(self):
+        print(f"Я загадал число {self.hidden_number}. Это число больше, меньше или равно с загаданному Вами числу?")
+        answer = input(r"Ответ должен быть одним из трех вариантов: <, > или =. Введите ответ:")
+        match answer:
+            case '<':
+                result = 'Less'
+                self.range_start = self.hidden_number + 1
+            case '>':
+                result = 'More'
+                self.range_end = self.hidden_number - 1
+            case '=':
+                result = 'Win'
+            case _:
+                print(r"Ответ не является одним из трех допустимых вариантов: <, > или =.")
+                result = self.get_answer()
+        if self.range_start > self.range_end:
+            self.range_start = self.range_end
+            result = 'Win'
+        self.hidden_number = randint(self.range_start, self.range_end)
+        return result
--- main.py
+++ main.py
@@ -1,66 +1,7 @@
-import random
+from GameEngine import GameVsPlayer, PlayerVsGame
 
+game1 = GameVsPlayer()
+game1.start_game()
+# game2 = PlayerVsGame()
+# game2.start_game()
 
-class Game:
-    def __init__(self):
-        self.hidden_number: int = 0
-        self.user_number: int = 0
-        self.range_from: int = 1
-        self.range_to: int = 10
-
-    def set_range(self, first_number: int, last_number: int) -> 'Успешно ли обновили диапазон: bool':
-        if isinstance(first_number, int):
-            if isinstance(last_number, int):
-                self.range_from = first_number
-                self.range_to = last_number
-                return True
-        return False
-
-    def __randomise_number(self):
-        random.seed()
-        self.hidden_number = random.randint(self.range_from, self.range_to)
-
-    def __get_answer(self) -> int:
-        try:
-            user_answer = int(input(r"Введите число: "))
-            if user_answer <= self.range_to:
-                if user_answer >= self.range_from:
-                    return user_answer
-            print(f"Введенная строка не является целым числом от {self.range_from} до {self.range_to}. "
-                  f"Попробуйте угадать снова.")
-        except ValueError:
-            print(f"Введенная строка не является целым числом от {self.range_from} до {self.range_to}. "
-                  f"Попробуйте угадать снова.")
-        return self.__get_answer()
-
-    def start(self):
-        self.__randomise_number()
-        print(f"Загадано целое число от {self.range_from} до {self.range_to}, попробуйте угадать.")
-        self.user_number = self.__get_answer()
-        while self.user_number != self.hidden_number:
-            if self.user_number < self.hidden_number:
-                print(f"Введенное число {self.user_number} меньше загаданного числа "
-                      f"от {self.range_from} до {self.range_to}, попробуйте угадать снова.")
-            else:
-                print(f"Введенное число {self.user_number} больше загаданного числа "
-                      f"от {self.range_from} до {self.range_to}, попробуйте угадать снова.")
-            self.user_number = self.__get_answer()
-        print(f"Было загадано число {self.hidden_number}. Вы победили. Попробовать снова?")
-        if input(r"Да/Нет: ") == 'Да':
-            self.start()
-
-
-game = Game()
-if game.set_range(1, 15):
-    game.start()
-
-# Загадано целое число от 1 до 10, попробуйте угадать.
-# Введите число: 4
-# Введенное число 4 меньше загаданного числа от 1 до 10, попробуйте угадать снова.
-# Введите число: 6
-# Введенное число 6 больше загаданного числа от 1 до 10, попробуйте угадать снова.
-# Введите число: АБВ
-# Введенная строка не является целым числом от 1 до 10. Попробуйте угадать снова.
-# Введите число: 5
-# Было загадано число 5. Вы победили. Попробовать снова?
-# Да/Нет:
